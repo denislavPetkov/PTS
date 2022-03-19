8207064ddc9d8e573d48c971710e034ceb13f589
denislav <denislav1396@gmail.com>
2022-03-19 15:32:40 +0200

    Add new class and interface

---
+++ src/main/java/com/github/tuproject/pts/service/serviceImpl/SummarizeResultsImpl.java
@@ -0,0 +1,6 @@
+package com.github.tuproject.pts.service.serviceImpl;
+
+import com.github.tuproject.pts.service.SummarizeResults;
+
+public class SummarizeResultsImpl implements SummarizeResults {
+}
